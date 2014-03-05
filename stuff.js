a5947933d9f2c54d71d996d9f85f2977905ffccb
Chris Matheson <chris.matheson@ivendi.com>
2014-03-05 13:34:26 +0000

    getting close to finished

--- stuff.js
+++ stuff.js
@@ -1,5 +1,5 @@
 /*globals angular, moment, document*/
-angular.module('app', ['ivYearsMonths'])
+angular.module('app', ['ivYearsMonths', 'iv.utils'])
     .directive('countup', function($interval) {
         return {
           restrict: 'EA',
@@ -15,78 +15,6 @@ angular.module('app', ['ivYearsMonths'])
                   scope.seconds = moment(diff).format('s');
               }, interval);
           }
-        };
-    })
-    .directive('input', function () {
-        'use strict';
-
-        return {
-            restrict: 'E',
-            require: '?ngModel',
-
-            link: function postLink(scope, element, attr, ngModel) {
-                if (!ngModel || attr.type !== 'sortcode') {
-                    return;
-                }
-
-                var regex = /[0-9]{2}[0-9]{2}[0-9]{2}/;
-
-                function validate(inputString) {
-                    var validity = regex.test(inputString) && inputString.length === 6;
-                    ngModel.$setValidity('sortcode', validity);
-                    return inputString;
-                }
-
-                function stripDashes(inputString) {
-                    return inputString.replace(/-/g, '');
-                }
-
-                ngModel.$parsers.push(stripDashes);
-                ngModel.$parsers.push(validate);
-            }
-        };
-    })
-    .directive('minAge', function () {
-        'use strict';
-
-        return {
-            restrict: 'A',
-            require: ['ngModel', 'timestampDate'],
-            link: function (scope, element, attrs, ctrls) {
-                var ngModel, min;
-                ngModel = ctrls[0];
-
-                min = parseInt(attrs.minAge, 10);
-
-                if (!angular.isNumber(min)) {
-                    throw new Error('min-value must be a number');
-                }
-
-                function isOverMaxValue(val) {
-                    //ignore empty strings
-                    if (!val) {
-                        return val;
-                    }
-                    var datum, testVal;
-
-                    datum = moment().subtract('y', min);
-                    testVal = moment(val);
-
-                    if (!datum.isValid() || !testVal.isValid()) {
-                        return undefined;
-                    }
-
-                    if (testVal.isAfter(datum)) {
-                        ngModel.$setValidity('min-age', false);
-                        return undefined;
-                    }
-
-                    ngModel.$setValidity('min-age', true);
-                    return val;
-                }
-
-                ngModel.$parsers.push(isOverMaxValue);
-            }
         };
     });
 
